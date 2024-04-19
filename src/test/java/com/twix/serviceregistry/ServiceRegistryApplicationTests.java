75063af92ec835f5643f67b385da87aab6fcb650
I489394 <servetcan.celik@student.fontys.nl>
2024-04-19 19:35:18 +0200

    Initial commit

---
+++ src/test/java/com/twix/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twix.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
